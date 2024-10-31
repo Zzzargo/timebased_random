9f9ebba02f8ef78abddf699b8e03e8d1b22b2464
Vasea <zzzargoman@gmail.com>
2024-10-31 13:07:18 +0200

    Changed the initial time from the time the program has been opened to the time the program actually starts outputting numbers.

--- timebased_random.c
+++ timebased_random.c
@@ -3,13 +3,13 @@
 
 int main()
 {
+    printf("Cate numere doriti? (0-255) ");
     time_t t0 = time(NULL);
     time_t t1 = time(NULL);
     int a;
     int start = t0;
     srand(time(NULL));
 
-    printf("Cate numere doriti? (0-255) ");
     scanf("%d", &a);
     while (t1 < start + a)
     {
